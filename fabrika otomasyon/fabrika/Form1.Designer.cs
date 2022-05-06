7952a037f80e157f7bd59d85200e0b66114c109a
AtlatanEray <97169303+AtlatanEray@users.noreply.github.com>
2022-05-06 14:27:39 +0300

    Add files via upload

---
+++ fabrika otomasyon/fabrika/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace fabrika
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.giris = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.Name = "textBox1";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.Window;
+            resources.ApplyResources(this.textBox2, "textBox2");
+            this.textBox2.Name = "textBox2";
+            // 
+            // giris
+            // 
+            this.giris.BackColor = System.Drawing.Color.SeaGreen;
+            resources.ApplyResources(this.giris, "giris");
+            this.giris.ForeColor = System.Drawing.Color.White;
+            this.giris.Name = "giris";
+            this.giris.UseVisualStyleBackColor = false;
+            this.giris.Click += new System.EventHandler(this.giris_Click);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.Controls.Add(this.giris);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button giris;
+    }
+}
+
---
+++ fabrika otomasyon/fabrika/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace fabrika
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void giris_Click(object sender, EventArgs e)
+        {
+            girisyap();
+        }
+
+        private void girisyap() 
+        { 
+            FabrikaEntities fabrikaEntities = new FabrikaEntities();
+
+            var kontrol = from kullanici in fabrikaEntities.Kullanici
+                          where kullanici.kullanıcı_adi == textBox1.Text && kullanici.sifre == textBox2.Text
+                          select kullanici;
+            if (kontrol.Any())
+            {
+                MessageBox.Show("Giriş Yapıldı");
+            }
+            else
+            {
+                MessageBox.Show("Hatalı Giriş");
+            }
+        
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        
+    }
+}
